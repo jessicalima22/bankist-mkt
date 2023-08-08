7546c19dd6dcd5f6e556c897386b7bf0010961f9
jessicalima22 <jessica.lima22@hotmail.com>
2023-08-08 20:35:17 -0300

    smooth scrolling nav bar

--- script.js
+++ script.js
@@ -30,6 +30,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+//button scrolling
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 btnScrollTo.addEventListener('click', function (e) {
@@ -42,4 +43,12 @@ btnScrollTo.addEventListener('click', function (e) {
     behavior: 'smooth',
   });*/
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+//page navigation
+
+document.querySelectorAll('nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    console.log('LINK');
+  });
 });
