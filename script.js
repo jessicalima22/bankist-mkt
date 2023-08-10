3c210cbae7a1f97b208f78563b0538b74325b809
jessicalima22 <jessica.lima22@hotmail.com>
2023-08-10 19:44:10 -0300

    get attribute

--- script.js
+++ script.js
@@ -50,6 +50,6 @@ btnScrollTo.addEventListener('click', function (e) {
 document.querySelectorAll('nav__link').forEach(function (el) {
   el.addEventListener('click', function (e) {
     e.preventDefault();
-    console.log('LINK');
+    const id = this.getAttribute('href');
   });
 });
