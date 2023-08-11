728ae3e9b5eb71253306a89309cc42d4d4fc5bb5
jessicalima22 <jessica.lima22@hotmail.com>
2023-08-11 19:31:14 -0300

    finish smooth scrolling

--- script.js
+++ script.js
@@ -47,9 +47,12 @@ btnScrollTo.addEventListener('click', function (e) {
 
 //page navigation
 
-document.querySelectorAll('nav__link').forEach(function (el) {
+document.querySelectorAll('.nav__link').forEach(function (el) {
   el.addEventListener('click', function (e) {
     e.preventDefault();
     const id = this.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
   });
 });
