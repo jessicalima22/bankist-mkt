94a1acf19764bbba797dc878443b780a576214ca
jessicalima22 <jessica.lima22@hotmail.com>
2023-08-30 00:22:41 -0300

    fixed functions, btn right and left ok

--- script.js
+++ script.js
@@ -150,7 +150,7 @@ const sectionObserver = new IntersectionObserver(revealSection, {
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden');
+  //section.classList.add('section--hidden');
 });
 
 //lazy loading
@@ -183,43 +183,41 @@ imgTargets.forEach(img => imgObserver.observe(img));
 //Slider
 
 const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
 
 let currentSlide = 0;
 const maxSlide = slides.length;
 
-const goToSlide = function () {
+const goToSlide = function (slide) {
   slides.forEach(
-    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)})%`)
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
   );
 };
 
 goToSlide(0);
 
-const btnLeft = document.querySelector('.slider__btn--left');
-const btnRight = document.querySelector('.slider__btn--right');
-
 //next slide
 
 const nextSlide = function () {
-  {
-    if (currentSlide === maxSlide - 1) {
-      currentSlide = 0;
-    } else {
-      currentSlide++;
-    }
-
-    goToSlide(currentSlide);
+  if (currentSlide === maxSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
   }
 
-  const prevSlide = function () {
-    if (currentSlide === 0) {
-      currentSlide = maxSlide - 1;
-    } else {
-      currentSlide--;
-    }
-    goToSlide(currentSlide);
-  };
+  goToSlide(currentSlide);
 };
+
+const prevSlide = function () {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+};
+
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
 
